1ed1159ace60e9924361cd8ed6f566fb836befb7
Kreshnik Hasanaj <kreshnik.hasanaj@gmail.com>
2017-05-16 23:30:14 +0200

    Initial commit.

---
+++ resources/assets/js/app.js
@@ -0,0 +1,2 @@
+require('./core/bootstrap');
+require('./core/plugins');
---
+++ resources/assets/js/core/bootstrap.js
@@ -0,0 +1,3 @@
+window._ = require('lodash');
+window.$ = window.jQuery = require('jquery');
+window.Promise = require('promise');
